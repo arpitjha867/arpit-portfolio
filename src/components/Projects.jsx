02e1f11ac12f3bce7a6cd7d4115198febfceecab
Arpit Jha <arpitjha867@gmail.com>
2022-10-31 12:44:08 +0530

    added links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -53,12 +53,12 @@ export default function Projects() {
                 {/* Container */}
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                     <div style={{backgroundImage : `url(${isstracker})`}}  className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                        <div className='opacity-0 group-hover:opacity-100'>
+                        <div className='opacity-0 group-hover:opacity-100 hover:duration-500'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
                                 International Space Station Tracker in 3D
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/' target='_blank'>
+                                <a href='https://drive.google.com/file/d/1vx-aCTv9msNII1sOm_6kRE63rr8CNZty/view?usp=sharing' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/arpitjha867/Space_Busters_Project' target='_blank'>
@@ -73,7 +73,7 @@ export default function Projects() {
                                 Travel Website
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/' target='_blank'>
+                                <a href='https://arpitjha867.github.io/Travel-Website-React-Tailwind/' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/arpitjha867/Travel-Website-React-Tailwind' target='_blank'>
@@ -88,7 +88,7 @@ export default function Projects() {
                                 Flipkart Clone Webapp
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/' target='_blank'>
+                                <a href='https://drive.google.com/file/d/17e0K-wiME02qPIq3pI-neVmJEnbzlkmi/view?usp=sharing' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/arpitjha867/flipkart-clone' target='_blank'>
@@ -103,7 +103,7 @@ export default function Projects() {
                                 Newsify Webapp
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/' target='_blank'>
+                                <a href='https://drive.google.com/file/d/1BGE6ET5TnGxMFFG-QuuSlcl1gDTEtjQh/view?usp=sharing' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
                                 <a href='https://github.com/arpitjha867/Newsify' target='_blank'>
@@ -118,10 +118,10 @@ export default function Projects() {
                                 Portfolio Website
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href='/' target='_blank'>
+                                <a href='https://635eb9681beb5469f600e9ba--flourishing-manatee-2c4fd2.netlify.app/' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                 </a>
-                                <a href='/' target='_blank'>
+                                <a href='https://github.com/arpitjha867/arpit-portfolio' target='_blank'>
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                 </a>
                             </div>
