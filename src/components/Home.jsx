8563e1ec43d86ceec7a3331cbeba0889da0b2d64
Arpit Jha <arpitjha867@gmail.com>
2022-10-31 18:03:42 +0530

    debugging

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 export default function Home() {
   return (
     <div  name="home" className='w-full h-screen bg-[#0a192f]'>
@@ -10,8 +11,14 @@ export default function Home() {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer,</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Frontend Developer specializing in building &#40; and occasionally designing &#41; exceptional digital experiences.</p>
         <div>
-            <button className='group hover:bg-[#5E17EB]  hover:border-[#5e17eb] duration-300 flex flex-row text-white border-2 px-6 py-3 my-2 items-center'>View Work
+            <button className='group hover:bg-[#5E17EB]  hover:border-[#5e17eb] duration-300 flex flex-row text-white border-2 px-6 py-3 my-2 items-center'>
+                    <Link  to="projects"  smooth={true} offset={50} duration={500} >
+                        View Work
+                    </Link>
                 <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-2'/> </span>
+            </button>
+            <button className='group hover:bg-[#5E17EB]  hover:border-[#5e17eb] duration-300 flex flex-row text-white border-2 px-6 py-3 my-2 items-center'>
+                <a href="https://drive.google.com/file/d/1gKTKCnlgWtu1xhWbIyOVqjtC3KBSeWAC/view?usp=sharing" target='_blank'>Resume</a>
             </button>
         </div>
       </div>
