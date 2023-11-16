88bae6af15e5b289be207282fe031895afd117b9
uc-apt <arpit.jha@ucertify.com>
2023-11-16 12:53:21 +0530

    skills update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,63 @@
 import React from "react";
 
+const skills = [
+  {
+    name: "JavaScript",
+    img: require("../assets/javascript.png"),
+  },
+  {
+    name: "React.js",
+    img: require("../assets/react.png"),
+  },
+  {
+    name: "Redux",
+    img: require("../assets/redux.png"),
+  },
+  {
+    name: "Node.js",
+    img: require("../assets/nodejs.png"),
+  },
+  {
+    name: "Express.js",
+    img: require("../assets/express.png"),
+  },
+  {
+    name: "MongoDB",
+    img: require("../assets/mongodb.png"),
+  },
+  {
+    name: "PHP",
+    img: require("../assets/php.png"),
+  },
+  {
+    name: "Tailwind",
+    img: require("../assets/tailwind.png"),
+  },
+  {
+    name: "HTML",
+    img: require("../assets/html.png"),
+  },
+  {
+    name: "CSS",
+    img: require("../assets/css.png"),
+  },
+  {
+    name: "GitHub",
+    img: require("../assets/github.png"),
+  },
+];
+
+const SkillTab = ({ name, img }) => {
+  return (
+    <>
+      <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+        <img className="w-20 mx-auto" src={img} alt="html" />
+        <p className="my-4">{name}</p>
+      </div>
+    </>
+  );
+};
+
 export default function Skills() {
   return (
     <div name="skills" className="bg-[#0a192f] text-gray-300 w-full h-screen">
@@ -12,70 +70,9 @@ export default function Skills() {
           <p className="py-4">/* Technologies I've worked with */</p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/javascript.png")}
-              alt="html"
-            />
-            <p className="my-4">Javascript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/react.png")}
-              alt="html"
-            />
-            <p className="my-4">React</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/redux.png")}
-              alt="html"
-            />
-            <p className="my-4">Redux</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 flex flex-col justify-evenly">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/php.png")}
-              alt="html"
-            />
-            <p className="my-4">PHP</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/html.png")}
-              alt="html"
-            />
-            <p className="my-4">HTML</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/css.png")}
-              alt="html"
-            />
-            <p className="my-4">CSS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/tailwind.png")}
-              alt="html"
-            />
-            <p className="my-4">Tailwind</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img
-              className="w-20 mx-auto"
-              src={require("../assets/github.png")}
-              alt="html"
-            />
-            <p className="my-4">GitHub</p>
-          </div>
+          {skills.map((skil) => (
+            <SkillTab {...skil} />
+          ))}
         </div>
       </div>
     </div>
